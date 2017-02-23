afe6cf3c1a93efd1c74782bf876b338d7cb8cb2f
Dan Jesus <danper.jesus@gmail.com>
2017-02-23 14:21:57 -0300

    Move all code to functional paradigm

--- index.js
+++ index.js
@@ -4,130 +4,128 @@ const DEFAULT_RULE = 'defaultRule';
 const DROPDOWN_RULE = 'dropdownRule';
 const UPLOAD_RULE = 'uploadRule';
 
-class PercentageDisplay {
-  calculate(mainStructure, data) {
+function calculatePercentage(mainStructure, data) {
+  const result = [];
 
-    const result = [];
+  mainStructure.survsAreaList.forEach(area => {
+    let areaPercentual = 0;
+    let questionWeigth = 1 / area.structure.all.length;
 
-    mainStructure.survsAreaList.forEach(area => {
-      let areaPercentual = 0;
-      let questionWeigth = 1 / area.structure.all.length;
+    area.structure.all.forEach(structure => {
+      if (!structure.parent) {
+        return;
+      }
 
-      area.structure.all.forEach(structure => {
-        if (!structure.parent) {
-          return;
-        }
+      areaPercentual += calculateQuestion(structure, data);
+    });
 
-        areaPercentual += this.calculateQuestion(structure, data);
+    if (area.structure.all[0].parent) {
+      result.push({
+        area: area.name,
+        percentage: (questionWeigth * areaPercentual) * 100
       });
-
-      if (area.structure.all[0].parent) {
-        result.push({
-          area: area.name,
-          percentage: (questionWeigth * areaPercentual) * 100
-        });
-      }
-
-    });
-
-    return result;
-  }
-
-  calculateQuestion(structure, data) {
-    let requiredQuestions = structure.input.filter(question => {
-      if (!(question.description_input || question).hasOwnProperty('required_question')) {
-        return false;
-      }
-
-      return question.required_question || question.description_input.required_question;
-    });
-    let rule = structure.parent.rule;
-
-    let requiredQuestionsTotal = structure.parent.min_required || requiredQuestions.length;
-
-    if (rule === DROPDOWN_RULE) {
-      requiredQuestionsTotal = requiredQuestions.length;
     }
 
-    requiredQuestionsTotal = requiredQuestionsTotal > 0 ? requiredQuestionsTotal : 1;
+  });
 
-    let questionWeigth = 1 / requiredQuestionsTotal;
-    let currentPercentage = 0;
+  return result;
+}
 
-    requiredQuestions.forEach(question => {
-      let questionArr = question.model.split('$index.');
+function calculateQuestion(structure, data) {
+  let requiredQuestions = structure.input.filter(question => {
+    if (!(question.description_input || question).hasOwnProperty('required_question')) {
+      return false;
+    }
 
-      if (questionArr.length === 1) {
-        questionArr = question.model.split('.');
-      }
+    return question.required_question || question.description_input.required_question;
+  });
+  let rule = structure.parent.rule;
+  let requiredQuestionsTotal = structure.parent.min_required || requiredQuestions.length;
 
-      if (questionArr.length > 1) {
+  if (rule === DROPDOWN_RULE) {
+    requiredQuestionsTotal = requiredQuestions.length;
+  }
 
-        if (rule === ROW_TEMPLATE_RULE) {
-          currentPercentage = 0;
+  requiredQuestionsTotal = requiredQuestionsTotal > 0 ? requiredQuestionsTotal : 1;
 
-          for(let i = 1; i <= structure.parent.min_required; i++) {
+  let questionWeigth = 1 / requiredQuestionsTotal;
+  let currentPercentage = 0;
 
-            if (!data[questionArr[0] + i]) {
-              return;
-            }
+  requiredQuestions.forEach(question => {
+    let questionArr = question.model.split('$index.');
 
-            let questionData = data[questionArr[0] + i][questionArr[1]];
+    if (questionArr.length === 1) {
+      questionArr = question.model.split('.');
+    }
 
-            if (this.isValidQuestion(rule, questionData, question)) {
-              currentPercentage += questionWeigth;
-            }
+    if (questionArr.length > 1) {
+      if (rule === ROW_TEMPLATE_RULE) {
+        currentPercentage = 0;
+
+        for(let i = 1; i <= structure.parent.min_required; i++) {
+
+          if (!data[questionArr[0] + i]) {
+            return;
           }
-        } else {
 
-          let questionData = data[questionArr[0]] ? data[questionArr[0]][questionArr[1]] : false;
+          let questionData = data[questionArr[0] + i][questionArr[1]];
 
-          if (!questionData) {
-            return false;
-          }
-
-          questionData = data[questionArr[0]][(question.description_input || question).model.split('.')[1]];
-
-          if (this.isValidQuestion(rule, questionData, question)) {
+          if (isValidQuestion(rule, questionData, question)) {
             currentPercentage += questionWeigth;
           }
         }
       } else {
-        if (data[questionArr[0]]) {
-          if (this.isValidQuestion(rule, data[questionArr[0]], question)) {
-            currentPercentage += questionWeigth;
-          }
+
+        let questionData = data[questionArr[0]] ? data[questionArr[0]][questionArr[1]] : false;
+
+        if (!questionData) {
+          return false;
+        }
+
+        questionData = data[questionArr[0]][(question.description_input || question).model.split('.')[1]];
+
+        if (isValidQuestion(rule, questionData, question)) {
+          currentPercentage += questionWeigth;
         }
       }
-    });
-
-    return this.showPercentage(structure.parent.rule, currentPercentage);
-  }
-
-  isValidQuestion(rule, data, question) {
-
-    if (rule === DEFAULT_RULE || rule === DROPDOWN_RULE) {
-      return data.length >= (question.min_length || question.description_input.min_length);
-    }
-
-    if (rule === ROW_TEMPLATE_RULE || rule === UPLOAD_RULE) {
-      return data || data > 0;
-    }
-
-    return false;
-  }
-
-  showPercentage(rule, percentage) {
-    if (rule === DEFAULT_RULE) {
-      if (percentage >= 1) {
-        return percentage;
+    } else {
+      if (data[questionArr[0]]) {
+        if (isValidQuestion(rule, data[questionArr[0]], question)) {
+          currentPercentage += questionWeigth;
+        }
       }
-
-      return  0;
     }
+  });
 
-    return percentage;
-  }
+  return showPercentage(structure.parent.rule, currentPercentage);
 }
 
-module.exports = new PercentageDisplay();
+function isValidQuestion(rule, data, question) {
+
+  if (rule === DEFAULT_RULE || rule === DROPDOWN_RULE) {
+    return data.length >= (question.min_length || question.description_input.min_length);
+  }
+
+  if (rule === ROW_TEMPLATE_RULE || rule === UPLOAD_RULE) {
+    return data || data > 0;
+  }
+
+  return false;
+}
+
+function showPercentage(rule, percentage) {
+  if (rule === DEFAULT_RULE) {
+    if (percentage >= 1) {
+      return percentage;
+    }
+
+    return  0;
+  }
+
+  return percentage;
+}
+
+module.exports = {
+  calculateQuestion,
+  calculatePercentage
+};
